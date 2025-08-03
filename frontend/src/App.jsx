782e249b054729b760128e362be204de7bb9e74d
Nafiuls <snafiul700@gmail.com>
2025-08-04 01:40:14 +0600

    fixed tailwind problem

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const App = () => {
-  return <div>Hello world</div>;
+  return (
+    <div>
+      <p className="text-4xl">hellow</p>
+    </div>
+  );
 };
 
 export default App;
